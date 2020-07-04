a6619bd14087a6bd62c6dce6f1d30c83875498ea
Steven Contreras <SFContra@gmail.com>
2020-07-03 23:20:03 -0700

    Implemented Bootstrap with containers and grids

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -10,9 +10,12 @@ class App extends React.Component{
 
     render() {
       return (
-        <div>
-          <header>
-            <a>Home</a>
+        <div class="container-fluid">
+          <header class="row">
+            <a class="col">Home</a>
+            <a class="col">About</a>
+            <a class="col">Projects</a>
+            <a class="col">Contact</a>
           </header>
           <main>
             <h1>Steven Contreras</h1>
