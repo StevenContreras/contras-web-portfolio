a0dac5024ba469383fc39a3dbef180cb81fc23f1
Steven Contreras <SFContra@gmail.com>
2020-09-24 11:51:53 -0700

    updated projects layout

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -18,27 +18,27 @@ class App extends React.Component{
           <a class="col">Contact</a>
         </nav>
         <main>
-          <article class="intro section" >
+          <header class="section">
             <h1 class="h">Steven Contreras</h1>
             <h2 class="content">Software Engineer</h2>
-          </article>
-          <article class="section">
+          </header>
+          <section class="section">
             <h2 class="h">About</h2>
             <p class="content" class="lead">sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </article>
-          <article class="section">
+          </section>
+          <section class="section">
             <h2 class="h">Projects</h2>
-            <ul class="content">
-              <li>Coming Soon</li>
-              <li>Coming Soon</li>
-              <li>Coming Soon</li>
-              <li>Coming Soon</li>
-            </ul>
-          </article>
-          <article class="section">
+            <section class="content">
+              <h3>Project Name</h3>
+              <img src="" alt="image or gif of project"></img>
+              <p>Summary/description of the project and or your role in it.</p>
+              <a href="#" target="_blank">Link to project</a>
+            </section>
+          </section>
+          <section class="section">
             <h2 class="h">Contact</h2>
             <address class="content">sfcontra@gmail.com</address>
-          </article>
+          </section>
         </main>
         <footer>
           <a class="cred" href="https://github.com/StevenContreras">GitHub</a>
