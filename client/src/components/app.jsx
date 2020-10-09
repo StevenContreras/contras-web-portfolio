17da99a3783cf2c1ff18a5cf829936ed3c0bb139
Steven Contreras <SFContra@gmail.com>
2020-10-09 14:27:20 -0700

    Move serape aside and recolor/adjust

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -21,24 +21,29 @@ class App extends React.Component {
   
   render() {
     return (
-      <div class="plate">
-        <nav className="row">
-          <button className="col" value={0} onClick={e => this.handleClick(e)}>Home</button>
-          <button className="col" value={1} onClick={e => this.handleClick(e)}>Projects</button>
-        </nav>
-        <main>
-          {this.state.nav === 0
-            ?  <Home></Home> 
-            :  <Projects></Projects>
-          }
-        </main>
-        <footer>
-          <a className="cred" href="mailto:sfcontra@gmail.com">
-            <img name="mail"></img>
-          </a>
-          <a className="cred" href="https://github.com/StevenContreras">GitHub</a>
-          <a className="cred" href="https://www.linkedin.com/in/steven-contreras-358b4a73/">LinkedIn</a>
-        </footer>
+      <div class="body">
+        <div class="aside">
+
+        </div>
+        <div class="plate">
+          <nav className="row">
+            <button className="col" value={0} onClick={e => this.handleClick(e)}>Home</button>
+            <button className="col" value={1} onClick={e => this.handleClick(e)}>Projects</button>
+          </nav>
+          <main>
+            {this.state.nav === 0
+              ?  <Home/> 
+              :  <Projects/>
+            }
+          </main>
+          <footer>
+            <a className="cred" href="mailto:sfcontra@gmail.com">
+              <img name="mail"></img>
+            </a>
+            <a className="cred" href="https://github.com/StevenContreras">GitHub</a>
+            <a className="cred" href="https://www.linkedin.com/in/steven-contreras-358b4a73/">LinkedIn</a>
+          </footer>
+        </div>
       </div>
     );
   }
