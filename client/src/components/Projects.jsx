ba996ce1e1ef8b173075a7e34266720c90aa1f62
Steven Contreras <SFContra@gmail.com>
2020-10-10 00:10:10 -0700

    Build Projects list

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -5,9 +5,27 @@ function Projects() {
     <section className="section">
       <h2 className="h">Projects</h2>
       <section className="content">
-        <h3 class="h">Project Name</h3>
+        <h3 class="h"><u>Slim Well</u></h3>
         <img src="" alt="image or gif of project"></img>
-        <p>Summary/description of the project and or your role in it.</p>
+        <p>Weight loss tracking app that monitors for a healthy weight loss pace.</p>
+        <a href="#" target="_blank">Link to project</a>
+      </section>
+      <section className="content">
+        <h3 class="h"><u>CSS Serape Generator</u></h3>
+        <img src="" alt="image or gif of project"></img>
+        <p>Randomly generates linear gradient Serape designs.</p>
+        <a href="#" target="_blank">Link to project</a>
+      </section>
+      <section className="content">
+        <h3 class="h"><u>Loteria</u></h3>
+        <img src="" alt="image or gif of project"></img>
+        <p>Mexican Bingo Game.</p>
+        <a href="#" target="_blank">Link to project</a>
+      </section>
+      <section className="content">
+        <h3 class="h"><u>Photography Portfolio Website</u></h3>
+        <img src="" alt="image or gif of project"></img>
+        <p>Website to portray a photographers portfolio</p>
         <a href="#" target="_blank">Link to project</a>
       </section>
     </section>
